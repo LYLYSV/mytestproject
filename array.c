b8c4236832725825db809c12faa0f4f9609753a1
LYLYSV <linda10.yeh@gmail.com>
2025-11-07 12:35:52 -0700

    4th commit

--- array.c
+++ array.c
@@ -6,46 +6,56 @@ int compare(const void *a, const void *b){
 	int val2 = *(int*)b;
 	printf("a[0] is %d,b[0] is %d\n", val1,val2);
 	printf("\n");
-	return ( val1 - val2 ); 
+	return ( val1 - val2 );
 }
 */
-int compare(const void *a, void *b){
+// test revert
+int compare(const void *a, void *b)
+{
 
-	return(*(int*)a - *(int*)b); 
+	return (*(int *)a - *(int *)b);
 }
-bool twosum(int arr[], int n, int target){
-	qsort(arr,n,sizeof(int),compare);
-/*	for (int i = 0; i < n; i++) {
-  		printf("%d\n", arr[i]);
-	}
-*/
-	int left =0, right = n-1;
-	
-	while(left<right){
-		int sum = arr[left]+arr[right];
-		if (sum == target){
+bool twosum(int arr[], int n, int target)
+{
+	qsort(arr, n, sizeof(int), compare);
+	/*	for (int i = 0; i < n; i++) {
+			printf("%d\n", arr[i]);
+		}
+	*/
+	int left = 0, right = n - 1;
+
+	while (left < right)
+	{
+		int sum = arr[left] + arr[right];
+		if (sum == target)
+		{
 			return true;
-		}else if (sum < target){
+		}
+		else if (sum < target)
+		{
 			left++;
-		}else{
+		}
+		else
+		{
 			right--;
 		}
-	
 	}
 	return false;
 }
 
-
-int main(){
+int main()
+{
 	int arr[] = {0, -1, 2, -3, 1};
 	int target = 2;
-	int n = sizeof(arr) / sizeof (arr[0]);
-    	printf("number in array is %d\n",n);
-	
-	if(twosum(arr,n,target)){
+	int n = sizeof(arr) / sizeof(arr[0]);
+	printf("number in array is %d\n", n);
+
+	if (twosum(arr, n, target))
+	{
 		printf("found target\n");
-	}else{
+	}
+	else
+	{
 		printf("can't found target\n");
 	}
-	    
 }
